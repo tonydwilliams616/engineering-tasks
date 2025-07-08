5453d30a7ed6b4f33f939f4d925ae18a449d6e5d
tonydwilliams101 <tonydwilliams101@gmail.com>
2025-07-08 12:42:03 +0100

    update the providers block

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -9,19 +9,6 @@ terraform {
   required_version = ">= 1.0"
 }
 
-# provider "aws" {
-#   region                      = "us-east-1"
-#   access_key                  = "test"
-#   secret_key                  = "test"
-#   skip_credentials_validation = true
-#   skip_metadata_api_check     = true
-#   skip_requesting_account_id  = true
-
-#   endpoints {
-#     s3 = "http://localhost:4566"
-#   }
-# }
-
 # Data source for current AWS account ID (for LocalStack)
 data "aws_caller_identity" "current" {}
 
